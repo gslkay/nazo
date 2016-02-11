a95fc158af04a49d2e15d8364a7c5a7167273c8e
gslkay <gslkay@gmail.com>
2016-02-11 23:58:50 +0900

    Update picture_upload.php

--- picture_upload.php
+++ picture_upload.php
@@ -2,11 +2,11 @@
 	$count = count($_FILES['upfile']['name']);
 	for($i = 0; $i < $count; $i++){
 		$name = htmlspecialchars(@$_FILES['upfile']['name'][$i]);
-		$uploadfile = "C:/nginx/html/uptest/picture/" . date('Ymd_H-i-s')."_" . $i .".". file_ext($name);
+		$uploadfile = "C:/nginx/html/uptest/picture/" . date('Ymd_H-i-s')."_" . $i .".". file_check($name, 1);
 
-		if(file_check($name)){
+		if(file_check($name, 0)){
 			if(move_uploaded_file(@$_FILES['upfile']['tmp_name'][$i], $uploadfile)){
-				print("<h1>success0</h1><br>");
+				print("<h1>success</h1><br>");
 			}
 			else{
 				print("<h1>false</h1><br>");
@@ -15,32 +15,27 @@
 		else{
 			print("<h1>false</h1><br>");
 		}
-			print(file_ext($name).'<br>');
-			print("<h2>success1</h2><br>");
-			print($_FILES['upfile']['tmp_name'][$i]."<br><br>");
 	}
 
-	function file_check($file){
+	function file_check($file, $num){
 		static $file_list = array('jpg', 'png');
 		$path = pathinfo($file);
 		$ext = strtolower($path['extension']);
 		if(in_array($ext, $file_list)){
-			return true;
+			if($num == 0){
+				return true;
+			}
+			elseif($num == 1){
+				return $ext;
+			}
 		}
 		else{
-			return false;
+			if($num == 0){
+				return false;
+			}
+			elseif($num == 1){
+				return $ext;
+			}
 		}
-	}
-	function file_ext($file){
-		static $file_name = array('jpg', 'png');
-		$path = pathinfo($file);
-		$ext = strtolower($path['extension']);
-		if(in_array($ext, $file_name)){
-			return $ext;
-		}
-		else{
-			return $ext;
-		}
-	}
-		
+	}	
 ?>
